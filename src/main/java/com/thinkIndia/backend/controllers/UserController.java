744ccfcf36e84454434a36d598d536f31a18aa82
Achir Jain <achirjain11@gmail.com>
2025-08-21 01:57:45 +0530

    add edit profile and change password functionality

--- src/main/java/com/thinkIndia/backend/controllers/UserController.java
+++ src/main/java/com/thinkIndia/backend/controllers/UserController.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,7 +18,6 @@ import com.thinkIndia.backend.entities.Images;
 import com.thinkIndia.backend.entities.User;
 import com.thinkIndia.backend.services.ImageService;
 import com.thinkIndia.backend.services.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 
@@ -28,13 +28,29 @@ public class UserController {
     private ImageService imageService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    @PostMapping("/uploadProfilePic/{email}")
-    public ResponseEntity<?> uploadProfilePic(@RequestParam(value="Profile_pic") MultipartFile imageFile, @PathVariable("image") String email) throws IOException {
+    @PostMapping("/editProfile/{email}")
+    public ResponseEntity<?> editProfile(@RequestParam(value="Profile_pic") MultipartFile imageFile,
+    @RequestParam(value="UserName") String name, @PathVariable("image") String email) throws IOException {
         
         User user = (User) userService.loadUserByUsername(email);
         int savedImageId = uploadImage(imageFile);
         user.setImageId(savedImageId);
+        user.setName(name);
+        userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping("/changePassword/{email}")
+    public ResponseEntity<?> changePassword(@RequestParam(value="Old_password") String oldPassword,@RequestParam(value="New_password") String newPassword, @PathVariable("image") String email) {
+        User user = (User) userService.loadUserByUsername(email);
+        oldPassword = passwordEncoder.encode(oldPassword);
+        if(!oldPassword.equals(user.getPassword())){
+            return new ResponseEntity<>("wrong old password.",HttpStatus.BAD_REQUEST);
+        }
+        newPassword = passwordEncoder.encode(newPassword);
+        user.setPassword(newPassword);
         userService.saveUser(user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
